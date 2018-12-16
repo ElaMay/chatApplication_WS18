1c0a4c4e13c810cd9bab1b8956b457d91f6b91d9
Sophia Weissenberger <weissenb@hm.edu>
2018-12-16 23:19:19 +0100

    AuditLogUDP

--- chatApplication/src/main/java/edu/hm/dako/chat/server/AuditLogPDU.java
+++ chatApplication/src/main/java/edu/hm/dako/chat/server/AuditLogPDU.java
@@ -1,4 +1,22 @@
 package edu.hm.dako.chat.server;
 
-public class AuditLogPDU {
+import java.io.Serializable;
+
+public class AuditLogPDU implements Serializable {
+
+    /**
+     * @author Sophia Weißenberger
+     *
+     * übertragen:
+     *
+     * - Typ der Nachricht
+     * - Zeitstempel der Erzeugung der AuditLog-Nachricht
+     * - Chat-Client-Name
+     * - Identifikation des verarbeitenden WorkerThreads im Chat-Server
+     * - Identifikation des verarbeitenden Threads im Chat-Client
+     * - Inhalt der Chat-Nachricht (nur bei Chat Message-Request)
+     * https://stackoverflow.com/questions/3997459/send-and-receive-serialize-object-on-udp
+     * */
+
+
 }
