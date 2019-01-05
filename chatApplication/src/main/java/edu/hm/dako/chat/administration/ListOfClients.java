ac3621a52c03e9b19da1e06bcbe815a431f9760c
Diana2402 <you@example.com>
2019-01-05 20:11:22 +0100

    Listen für die Anzahl der User erstellen. Container-klasse erstellen.

---
+++ chatApplication/src/main/java/edu/hm/dako/chat/administration/ListOfClients.java
@@ -0,0 +1,30 @@
+package edu.hm.dako.chat.administration;
+
+import java.util.ArrayList;
+
+
+public class ListOfClients {
+
+    //Namen der Clients, die dann aufgelistet werden.
+    private String clientName;
+
+
+    //Eine ArrayList wird für diese Klasse erzeugt.
+    ArrayList<String> clientList = new ArrayList<>();
+
+
+    //Konstruktor für diese Klasse.
+    public ListOfClients () {
+        this.clientName = clientName;
+    }
+
+
+    /**
+     * Zählt alle Clients in einer Liste zusammen und filtert alle Namen raus.
+     */
+    public void filter() {
+
+        // String[] clients = new String[];
+    }
+
+}
