194915e32c10f65c8b435812543f6189e6333190
Sophia Weissenberger <weissenb@hm.edu>
2019-01-05 19:45:57 +0100

    Neues Package mit neuen Klassen für das Admin-programm.

---
+++ chatApplication/src/main/java/edu/hm/dako/chat/administration/ClientStatistic.java
@@ -0,0 +1,22 @@
+package edu.hm.dako.chat.administration;
+
+public class ClientStatistic {
+
+    private String clientName;
+    private String loginTimestamp;
+    private int messageCounter;
+    private String logoutTimestamp;
+
+
+
+
+    public ClientStatistic(){
+        this.clientName = null;
+        this.loginTimestamp = null;
+        this.messageCounter = 0;
+        this.logoutTimestamp = null;
+    }
+
+
+
+}
